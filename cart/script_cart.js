f6e32b3c1319aea196ab9cf39e71588ab5e60dfc
Carole Bellet <belletcarole@yahoo.fr>
2023-04-12 10:41:20 +0200

    cart-frontend

--- cart/script_cart.js
+++ cart/script_cart.js
@@ -1,17 +1,18 @@
 // suite ajout trip
-function cartadded(){
+function cartAdded(){
   fetch(`http://localhost:3000/cart` )
   .then(response => response.json())
   .then(data => {
     if(data.carts.length === 0){
-      document.querySelector('#result_card').innerHTML = 
+      document.querySelector('#container').innerHTML = 
       `
-      <img src="/images/notfound.png">
-      <hr>
-      <p class='ErrorResult'>No trip found</p>
+      <p class='nocart'>No tickets in your cart.</p>
+      <p class='no cart'>Why not plan a trip ?</p>
+
       `
   } else {
-    const newTrip = ` 
+    document.querySelector('#container').innerHTML = 
+    const newCart = ` 
                 <div id="cart_trip">
                 <div>voyage</div>
                 <div>heure de départ</div>
@@ -20,19 +21,25 @@ function cartadded(){
 
             </div>
   
-`; }
+`; 
+// Somme du panier
+for (let i = 0; i < document.querySelectorAll('.price').length; i++) {
+   
+  let cartCount = Number(document.querySelector('.price').textContent);
+  
+  let total = 0;
+total += cartCount;
+console.log(cartCount);
+  document.querySelector('#count').textContent = total;
+    
+};
+}
   })
 };
 
 
 
 
-
-
-
-      
-
-//document.querySelector('#msg-container').innerHTML += newTrip;
 
 
 // Somme du panier
@@ -53,6 +60,8 @@ console.log(cartCount);
 
 
 // mise à jour suppression trip
+
+
 for (let i = 0; i < document.querySelectorAll('.btn-delete').length; i++) {
     document.querySelectorAll('.btn-delete')[i].addEventListener('click', function()
       {
@@ -97,42 +106,3 @@ for (let i = 0; i < document.querySelectorAll('.btn-purchase').length; i++) {
 
 
 
-
-
-function searchTrips(){
-  const departure = document.querySelector('#departure').value;
-  const arrival = document.querySelector('#arrival').value;
-  const date = document.querySelector('#search_date').value;
-  fetch(`http://localhost:3000/tripsFilter/${departure}/${arrival}/${date}`
-  )
-  .then(response => response.json())
-  .then(data => {
-      if(data.trips.length ===0){
-          document.querySelector('#result_card').innerHTML = 
-          `
-          <img src="/images/notfound.png">
-          <hr>
-          <p class='ErrorResult'>No trip found</p>
-          `
-      }
-      else{
-          document.querySelector('#result_card').innerHTML ='';
-          for(let dat in data){
-              for(let i =0; i<data[dat].length; i++){
-                  console.log(moment(data[dat][i].date).hour());
-              document.querySelector('#result_card').innerHTML +=
-          `<div id="ticket" >
-              <div id="trajet">
-                  ${data[dat][i].departure} > ${data[dat][i].arrival}
-              </div>
-              <div>${moment(data[dat][i].date).format('HH:mm')}</div>
-              <div>${data[dat][i].price}€</div>
-              <div><button>Book</button></div>
-      </div>`
-              }
-          }
-
-      }
-  })
-  
-}
