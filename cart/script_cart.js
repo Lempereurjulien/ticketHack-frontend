6e73ab59f92717e4f4dcbf376336916af47e1447
Carole Bellet <belletcarole@yahoo.fr>
2023-04-12 10:15:59 +0200

    cart added

--- cart/script_cart.js
+++ cart/script_cart.js
@@ -1,6 +1,18 @@
 // suite ajout trip
-const newTrip = ` 
-<div id="cart_trip">
+function cartadded(){
+  fetch(`http://localhost:3000/cart` )
+  .then(response => response.json())
+  .then(data => {
+    if(data.carts.length === 0){
+      document.querySelector('#result_card').innerHTML = 
+      `
+      <img src="/images/notfound.png">
+      <hr>
+      <p class='ErrorResult'>No trip found</p>
+      `
+  } else {
+    const newTrip = ` 
+                <div id="cart_trip">
                 <div>voyage</div>
                 <div>heure de départ</div>
                 <div class="price">130</div>
@@ -8,7 +20,17 @@ const newTrip = `
 
             </div>
   
-`;
+`; }
+  })
+};
+
+
+
+
+
+
+
+      
 
 //document.querySelector('#msg-container').innerHTML += newTrip;
 
@@ -71,4 +93,46 @@ for (let i = 0; i < document.querySelectorAll('.btn-purchase').length; i++) {
 
     }
   )
+}
+
+
+
+
+
+function searchTrips(){
+  const departure = document.querySelector('#departure').value;
+  const arrival = document.querySelector('#arrival').value;
+  const date = document.querySelector('#search_date').value;
+  fetch(`http://localhost:3000/tripsFilter/${departure}/${arrival}/${date}`
+  )
+  .then(response => response.json())
+  .then(data => {
+      if(data.trips.length ===0){
+          document.querySelector('#result_card').innerHTML = 
+          `
+          <img src="/images/notfound.png">
+          <hr>
+          <p class='ErrorResult'>No trip found</p>
+          `
+      }
+      else{
+          document.querySelector('#result_card').innerHTML ='';
+          for(let dat in data){
+              for(let i =0; i<data[dat].length; i++){
+                  console.log(moment(data[dat][i].date).hour());
+              document.querySelector('#result_card').innerHTML +=
+          `<div id="ticket" >
+              <div id="trajet">
+                  ${data[dat][i].departure} > ${data[dat][i].arrival}
+              </div>
+              <div>${moment(data[dat][i].date).format('HH:mm')}</div>
+              <div>${data[dat][i].price}€</div>
+              <div><button>Book</button></div>
+      </div>`
+              }
+          }
+
+      }
+  })
+  
 }
